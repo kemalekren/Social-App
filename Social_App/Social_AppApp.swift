32ec73f047c7f25331834128e75e60aacb7f81a9
kemalekren <kemalekren@hotmail.com>
2022-08-14 11:19:24 +0300

    Initial Commit

---
+++ Social_App/Social_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_AppApp.swift
+//  Social_App
+//
+//  Created by Kemal Ekren on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Social_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
